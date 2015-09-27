b5a2803fb8dac2aa29438a8e2704be3f95a5a92b
mvrolijk <miguel.vrolijk@gmail.com>
2015-09-27 09:24:24 -0400

    Added GNU GPL v3 license info.

--- ConvertPG2SS/ProcessBulk.cs
+++ ConvertPG2SS/ProcessBulk.cs
@@ -1,6 +1,22 @@
 ﻿//----------------------------------------------------------------------------------------
 // <copyright company="">
-//     Copyright Miguel Vrolijk (c). All rights reserved.
+//    Copyright 2015 Miguel Vrolijk.
+//
+//    This file is part of ConvertPG2SS.
+//
+//    ConvertPG2SS is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    ConvertPG2SS is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy (COPYING.txt) of the GNU General Public
+//    License along with ConvertPG2SS.  If not, see
+//    <http://www.gnu.org/licenses/>.
 // </copyright>
 // <author>Miguel Vrolijk</author> 
 // <date>2015-09-25</date>
